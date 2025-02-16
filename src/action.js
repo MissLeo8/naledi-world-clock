0a78b05e4ec9455b4977938fbba5661087a32a48
MissLeo8 <naledimasello@gmail.com>
2025-02-16 11:04:08 +0200

    added functionality for select element, fixed spelling in html

--- src/action.js
+++ src/action.js
@@ -18,5 +18,28 @@ johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
 johannesburgTimeElement.innerHTML = johannesburgTime.format("hh:mm:ss [<small>]A[</small>]");
 }
 
+
+function updatecity(event){
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split('/')[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+            <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div>
+            <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+            </div>
+        </div>
+        `;
+}
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+
+citySelectElement.addEventListener("change", updatecity);
+
