a88231077161f0a911bff4c0c0b6426f57d7a141
MissLeo8 <naledimasello@gmail.com>
2025-02-16 08:41:16 +0200

    removed fake data & added js functionality for johannesburg id

--- src/action.js
+++ src/action.js
@@ -1,5 +1,5 @@
 function updateTime(){
-
+// Milan
 let milanElement = document.querySelector("#milan");
 let milanDateElement = milanElement.querySelector(".date");
 let milanTimeElement = milanElement.querySelector(".time");
@@ -7,6 +7,16 @@ let milanTime = moment().tz("Europe/Rome");
 
 milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
 milanTimeElement.innerHTML = milanTime.format("hh:mm:ss [<small>]A[</small>]");
+
+// Johannesburg
+let johannesburgElement = document.querySelector("#johannesburg");
+let johannesburgDateElement = johannesburgElement.querySelector(".date");
+let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+johannesburgTimeElement.innerHTML = johannesburgTime.format("hh:mm:ss [<small>]A[</small>]");
 }
+
 updateTime();
 setInterval(updateTime, 1000);
