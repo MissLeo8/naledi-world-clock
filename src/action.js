2c254a3180fbee1b326b554d2a5c422e16bf1341
MissLeo8 <naledimasello@gmail.com>
2025-02-16 11:30:33 +0200

    added havana as additional city in html & included js functionality

--- src/action.js
+++ src/action.js
@@ -1,21 +1,36 @@
 function updateTime(){
-// Milan
-let milanElement = document.querySelector("#milan");
-let milanDateElement = milanElement.querySelector(".date");
-let milanTimeElement = milanElement.querySelector(".time");
-let milanTime = moment().tz("Europe/Rome");
+  // Milan
+  let milanElement = document.querySelector("#milan");
+  let milanDateElement = milanElement.querySelector(".date");
+  let milanTimeElement = milanElement.querySelector(".time");
+  let milanTime = moment().tz("Europe/Rome");
 
-milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
-milanTimeElement.innerHTML = milanTime.format("hh:mm:ss [<small>]A[</small>]");
+  milanDateElement.innerHTML = milanTime.format("MMMM Do YYYY");
+  milanTimeElement.innerHTML = milanTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
 
-// Johannesburg
-let johannesburgElement = document.querySelector("#johannesburg");
-let johannesburgDateElement = johannesburgElement.querySelector(".date");
-let johannesburgTimeElement = johannesburgElement.querySelector(".time");
-let johannesburgTime = moment().tz("Africa/Johannesburg");
+  // Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
 
-johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
-johannesburgTimeElement.innerHTML = johannesburgTime.format("hh:mm:ss [<small>]A[</small>]");
+  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  // Havana
+  let havanaElement = document.querySelector("#havana");
+  let havanaDateElement = havanaElement.querySelector(".date");
+  let havanaTimeElement = havanaElement.querySelector(".time");
+  let havanaTime = moment().tz("America/Havana");
+
+  havanaDateElement.innerHTML = havanaTime.format("MMMM Do YYYY");
+  havanaTimeElement.innerHTML = havanaTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
 }
 
 
